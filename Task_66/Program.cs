6858b04d01dc3cf6540b525af0522ff5af356a69
eN1nja <mr.buntar@gmail.com>
2023-09-04 12:37:55 +0300

    commit

---
+++ Task_66/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 66: Задайте значения M и N. 
+// Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+System.Console.Write("Введите число M: ");
+int num1 = Convert.ToInt32(System.Console.ReadLine());
+System.Console.Write("Введите число N: ");
+int num2 = Convert.ToInt32(System.Console.ReadLine());
+
+if (num1 < num2)
+{
+    Console.WriteLine($"Сумма натуральных чисел в промежутке от {num1} до {num2}: {PrintGetSum(num1, num2)}");
+}
+else
+{
+    System.Console.WriteLine("Введите число N больше M");
+}
+
+int PrintGetSum(int start, int end)
+{
+    if (start == end)
+    {
+        return start;
+    }
+    return start + PrintGetSum(start + 1, end);
+}
---
+++ Task_68/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+// Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+Console.Write("Введите число M: ");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+int Pow(int x, int y)
+{
+    if (x == 0)
+    {
+        return y + 1;
+    }
+    else if (y == 0)
+    {
+        return Pow(x - 1, 1);
+    }
+    else
+    {
+        return Pow(x - 1, Pow(x, y - 1));
+    }
+}
+Console.WriteLine($"Результат функции Аккермана для чисел {M} и {N}: {Pow(M, N)}");
